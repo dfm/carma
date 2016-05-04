a47c6ba57010095c5b5d5c4c4f73d4d54a572e48
Dan F-M <danfm@nyu.edu>
2016-05-03 22:08:36 -0700

    MCMC

--- carma/carma.py
+++ carma/carma.py
@@ -4,8 +4,8 @@ from __future__ import division, print_function
 
 import numpy as np
 
-from ._carma import carma_log_likelihood, carma_covariance
 from .conversions import sort_params, get_roots_from_params
+from ._carma import carma_log_likelihood, carma_covariance, carma_psd
 
 __all__ = ["CARMAModel"]
 
@@ -23,7 +23,11 @@ class CARMAModel(object):
         self.max_R0 = max_R0
 
     def log_prior(self):
-        if self.log_jitter < -12 or self.log_sigma < -12:
+        if not (-12 < self.log_jitter < 12 and -12 < self.log_sigma < 12):
+            return -np.inf
+        if np.any(self.arpars < -12) or np.any(self.arpars > 12):
+            return -np.inf
+        if np.any(self.mapars < -12) or np.any(self.mapars > 12):
             return -np.inf
         roots = get_roots_from_params(self.arpars)
         if not np.all(np.diff(roots.imag)[1::2] > 0.0):
@@ -66,17 +70,20 @@ class CARMAModel(object):
         return -self.log_probability(t, y, yerr)
 
     def get_vector(self):
-        return np.concatenate(([self.log_jitter, self.log_sigma],
+        return np.concatenate(([self.log_jitter],
                                self.arpars, self.mapars))
 
     def set_vector(self, vector):
         self.log_jitter = vector[0]
-        self.log_sigma = vector[1]
-        self.arpars = vector[2:2+self.p]
-        self.mapars = vector[2+self.p:2+self.p+self.q]
+        # self.log_sigma = vector[1]
+        self.arpars = vector[1:1+self.p]
+        self.mapars = vector[1+self.p:1+self.p+self.q]
 
     def sample(self, t):
         tau = t[:, None] - t[None, :]
         K = carma_covariance(self.log_sigma, self.arpars, self.mapars, tau)
         K[np.diag_indices_from(K)] += np.exp(2*self.log_jitter)
         return np.random.multivariate_normal(np.zeros_like(t), K)
+
+    def psd(self, f):
+        return carma_psd(self.log_sigma, self.arpars, self.mapars, f)
